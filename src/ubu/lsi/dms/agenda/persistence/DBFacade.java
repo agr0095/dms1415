5e578608b2294eec286cebfef7534f9c5898d56b
Alberto-Vivar <killer7129@gmail.com>
2014-11-04 17:40:52 +0100

    Todos los métodos declarados Creado el singleton

--- src/ubu/lsi/dms/agenda/persistence/DBFacade.java
+++ src/ubu/lsi/dms/agenda/persistence/DBFacade.java
@@ -1,15 +1,95 @@
-
 package ubu.lsi.dms.agenda.persistence;
 
+import java.util.List;
+
+import ubu.lsi.dms.agenda.modelo.Call;
+import ubu.lsi.dms.agenda.modelo.Contact;
+import ubu.lsi.dms.agenda.modelo.ContactType;
+
 public class DBFacade implements PersistenceFacade {
-	
-	public DBFacade getInstance(){
-		//TODO
+
+	private DBFacade singleInstance;
+
+	/**
+	 * Constructor de la fachada: hemos impedido su uso para seguir el patrón
+	 * singleton.
+	 */
+	private DBFacade() {
+		// Constructor no visible
+	}
+
+	/**
+	 * Devuelve la referencia a la única instancia que debería existir de la
+	 * DBFacade.
+	 * 
+	 * @return instancia de DBFacade.
+	 */
+	@Override
+	public PersistenceFacade createPersistenceFacade() {
+		if (singleInstance == null)
+			singleInstance = new DBFacade();
+		return singleInstance;
+	}
+
+	@Override
+	public Contact getContact(String surname) {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	private DBFacade(){
-		//Constructor no visible
+
+	@Override
+	public void insertContact(Contact contact) {
+		// TODO Auto-generated method stub
+
 	}
+
+	@Override
+	public void insertCall(Call call) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertContactType(ContactType ct) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateContact(Contact contact) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateCall(Call call) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateContactType(ContactType ct) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Contact> getContactsBySurname(String surname) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Call> getCallsByContact(Contact contacto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ContactType> getContactTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
