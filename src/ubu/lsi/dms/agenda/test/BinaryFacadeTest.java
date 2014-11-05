6a22e0ae315693d1b0814d57d0553d791732603a
ppp0015 <ppp0015@alu.ubu.es>
2014-11-05 19:57:35 +0100

    Añadida la clase BinaryFacadeTest.java

---
+++ src/ubu/lsi/dms/agenda/test/BinaryFacadeTest.java
@@ -0,0 +1,16 @@
+package ubu.lsi.dms.agenda.test;
+
+import ubu.lsi.dms.agenda.persistence.BinaryFacade;
+
+/**
+ * Clase que contiene tests para probar la persistencia
+ * con ficheros binarios.
+ * 
+ * @author Plamen
+ *
+ */
+public class BinaryFacadeTest {
+	public static void main(String args[]) {
+		
+	}
+}
